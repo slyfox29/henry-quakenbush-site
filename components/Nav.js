c2e6350066b7aa097ee9f56523eaa74219b50d9c
slyfox29 <147348295+slyfox29@users.noreply.github.com>
2025-02-21 20:59:39 -0500

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/contact">Contact</Link>
+    <nav className="nav">
+      <Link href="/" className="nav-link">Home</Link>
+      <Link href="/about" className="nav-link">About</Link>
+      <Link href="/blog" className="nav-link">Blog</Link>
+      <Link href="/calendar" className="nav-link">Calendar</Link>
     </nav>
   );
 }
