7897cf06fac247a68776d374130450eb1a44fdba
slyfox29 <147348295+slyfox29@users.noreply.github.com>
2025-02-21 20:48:37 -0500

    Create Nav.js

---
+++ components/Nav.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Nav() {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}
