39ddfadbe6f39c7b9d39e4e2b77def70782a0684
slyfox29 <147348295+slyfox29@users.noreply.github.com>
2025-02-21 20:50:42 -0500

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function About() {
+  return (
+    <Layout>
+      <section className="content">
+        <h2>About Me</h2>
+        <p>Hi, I’m Henry Quakenbush. I’m a [your profession/hobby] with a passion for [your interests]. This site is my digital home base.</p>
+      </section>
+    </Layout>
+  );
+}
